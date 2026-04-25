418cd2b5d158f5dec9349d8b6ee94a50d581a0fa
nicolas <nicolas.s.guim67@gmail.com>
2026-04-25 09:33:44 -0300

    fix: restaurando arquivos Controllers e Repositories

--- src/main/java/com/logica/financeira/logica/financeira/Repositories/CategoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.logica.financeira.logica.financeira.Repositories;
-
-import com.logica.financeira.logica.financeira.entities.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<Categoria,Long> {
-}
